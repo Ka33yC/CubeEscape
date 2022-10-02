6cd874d8595728601bd74e0e142db3a98dd7c7f6
Ka33yC <nkruzilin941@gmail.com>
2022-10-02 20:38:49 +0300

    Level starting created
    Требуется в переработке и доработке, но уже какой-никак, но есть результат

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using FigureGameObjects;
+using LevelGeneration;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public async void StartLevel(int levelNum)
+	{
+		await LoadScene(1);
+
+		FindObjectOfType<FigureSpawnerBase>().StartLevel(JsonConvert.DeserializeObject<LevelParameters>(
+			File.ReadAllText(Path.Combine(Application.streamingAssetsPath, $"lvl{levelNum}.json"))));
+		Destroy(gameObject);
+	}
+
+	public async void StartInfinityLevel(int levelNum)
+	{
+		await LoadScene(2);
+
+		FindObjectOfType<FigureSpawnerBase>().StartLevel(JsonConvert.DeserializeObject<LevelParameters>(
+			File.ReadAllText(Path.Combine(Application.streamingAssetsPath, $"infLvl{levelNum}.json"))));
+		Destroy(gameObject);
+	}
+
+	private async Task LoadScene(int sceneIndex)
+	{
+		var sceneLoadingOperation = SceneManager.LoadSceneAsync(sceneIndex);
+		sceneLoadingOperation.allowSceneActivation = true;
+		while (!sceneLoadingOperation.isDone)
+		{
+			await Task.Yield();
+		}
+	}
+}
